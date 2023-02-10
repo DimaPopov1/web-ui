2ca5a14cf16068a43fd38c6b63ab74c9fd42d29d
Dima <dima.popov@innowise-group.com>
2023-02-10 16:01:30 +0300

    Add features to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Carusel from "./Carusel";
 import './Home.css'
+import ContainerMarketing from "./ContainerMarketing";
 const Home = () => {
     return (
         <main role="main">
             <Carusel />
+            <ContainerMarketing/>
         </main>
     )
 }
