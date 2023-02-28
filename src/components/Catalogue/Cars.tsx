482508ac7b3560bd7ee8dff51c0e00d83e6d35a2
Dima <dima.popov@innowise-group.com>
2023-02-28 14:21:30 +0300

    magic on css

--- src/components/Catalogue/Cars.tsx
+++ src/components/Catalogue/Cars.tsx
@@ -1,14 +1,15 @@
 import React, {useEffect} from "react";
-import { ICar } from "../../types/types";
+import {ICar} from "../../types/types";
 
 import style from './Catalogue.module.css'
 
-const Cars = (props: {cars: ICar[] }) => {
+const Cars = (props: { cars: ICar[] }) => {
 
     const listCars = props?.cars?.map((car) =>
-        <div className="card col-sm-3 m-3 " key={car.id}>
-            <img src={["data:image/jpg;base64,", car.image].join("")}
-                 className="card-img img-thumbnail border-0" style={{height: 210}} alt="Image" />
+        <div className="card col-12 col-lg-3 col-sm-6 'col-sm-6' m-0 p-0 border-0 border-rad " key={car.id}>
+            <div className="m-2 rounded-2 " style={{backgroundColor:'#FDF9FE'}} >
+                <img src={["data:image/jpg;base64,", car.image].join("")}
+                     className="card-img img-fluid rounded-top rounded-1 " style={{height: 210, width:370}} alt="Image"/>
                 <div className="card-body">
                     <h5 className="card-title">{car.name}</h5>
                     <p className="card-text" style={{fontSize: "large"}}>
@@ -17,11 +18,13 @@ const Cars = (props: {cars: ICar[] }) => {
                     </p>
 
                 </div>
+            </div>
+
         </div>
     );
 
     return (
-        <div className="row justify-content-center">
+        <div className="row m-3 p-2  flex-wrap justify-content-between">
             {listCars}
         </div>
     )
--- src/components/Catalogue/SearchBar.tsx
+++ src/components/Catalogue/SearchBar.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const SearchBar = () => {
 
     return (
-        <div className="input-group col-sm-6    ">
+        <div className="input-group m-2 ">
             <input name="name" type="text" className="form-control" placeholder="Car's name"
                    aria-label="Recipient's username" aria-describedby="basic-addon2"
-                   value="ef"/>
+                   value=""/>
                 <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" type="submit" value="Search">Search</button>
+                    <button className="btn btn-outline-secondary m-0 " type="submit" value="Search">Search</button>
                 </div>
         </div>
     )
